d3790541b501992e741cf774b3af0d59d0430c60
blairfix <blairfix@fastmail.com>
2024-02-03 13:10:13 -0700

    more fields

--- pl/eprint_fields_bn.pl
+++ pl/eprint_fields_bn.pl
@@ -7,6 +7,51 @@ push @{$c->{fields}->{eprint}},
     'type' => 'text'
 },
 
+# month
+{ 'name' => 'month',
+    'type' => 'set',
+    'input_rows' => 1,
+    'options' => [ 'jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec' ]
+},
+
+# conference dates
+{
+    'maxlength' => 256,
+    'name' => 'confdates',
+    'type' => 'text'
+},
+
+# conference
+{
+    'name' => 'conference',
+    'type' => 'text'
+},
+
+# conference location
+{
+    'name' => 'confloc',
+    'type' => 'text'
+},
+
+# conference type
+{
+    'name' => 'conftype',
+    'type' => 'text'
+},
+
+# department
+{
+    'name' => 'department',
+    'type' => 'text'
+},
+
+
+
+
+
+
+
+
 
 # test field
 {
