c3777f52be10040c4907f5e40400c38b411fa8ce
blairfix <blairfix@fastmail.com>
2024-02-12 19:20:55 -0700

    added favicon

--- images/compress.sh
+++ images/compress.sh
@@ -1,6 +1,9 @@
 
-
+# compress logo
 convert  -resize 40% site_logo_20240212.png site_logo_20240212_cp.png
-
-
 mogrify -trim site_logo_20240212_cp.png
+
+# compress favicon
+convert  -resize 40% bn_favicon.png bn_favicon_cp.png
+
+
--- pl/branding.pl
+++ pl/branding.pl
@@ -1,5 +1,6 @@
 $c->{site_logo} = "/images/site_logo_20240212_cp.png";
 
+$c->{favicon} = "/images/bn_favicon.png";
+
 $c->{commons_logo} = "/images/somerights.gif";
 
-$c->{theme} = "bn_theme";
