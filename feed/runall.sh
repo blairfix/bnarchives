a10714b37a34189170ef2ffe280c568f05df994e
blairfix <blairfix@fastmail.com>
2024-02-20 11:59:33 -0700

    fix timers

--- feed/runall.sh
+++ feed/runall.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+# set script directory
+cd "$(dirname "$0")"
+
 # run R feed scripts
 for f in [0-9]*
 do
@@ -8,8 +11,7 @@ do
 done
 
 # generate html
-generate_static bnarchives
-#generate_views bnarchives
-#generate_abstracts bnarchives
+/opt/eprints3/bin/generate_static bnarchives
 
+# reload
 #epadmin reload --force bnarchives
--- timers/generate/generate.sh
+++ timers/generate/generate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-generate_views bnarchives
-generate_abstracts bnarchives
-generate_static bnarchives
+/opt/eprints3/bin/generate_views bnarchives
+/opt/eprints3/bin/generate_abstracts bnarchives
+/opt/eprints3/bin/generate_static bnarchives
