725a27c3f62d5c30e6e33a511eee9478bfffa354
blairfix <blairfix@fastmail.com>
2024-02-20 14:10:27 -0700

    thumnail script

--- timers/generate/generate.sh
+++ timers/generate/generate.sh
@@ -3,3 +3,12 @@
 /opt/eprints3/bin/generate_views bnarchives
 /opt/eprints3/bin/generate_abstracts bnarchives
 /opt/eprints3/bin/generate_static bnarchives
+
+
+# thumbnails
+head /home/eprints/feed/bn_urls.txt -n 2 | sed 's/[^0-9]*//g' > /tmp/bn_urls
+
+while read p; do
+    /opt/eprints3/bin/epadmin redo_thumbnails bnarchives $p --verbose
+done </tmp/bn_urls
+
