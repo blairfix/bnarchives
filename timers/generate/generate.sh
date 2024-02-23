2458a5637c455e2ed1d8961cd36e5ff5e6d9dc95
blairfix <blairfix@fastmail.com>
2024-02-23 14:59:33 -0700

    thumbs first

--- timers/generate/generate.sh
+++ timers/generate/generate.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/bash
 
-runuser -l eprints -c '/opt/eprints3/bin/generate_views bnarchives'
-runuser -l eprints -c '/opt/eprints3/bin/generate_abstracts bnarchives'
-runuser -l eprints -c '/opt/eprints3/bin/generate_static bnarchives'
-
 
 # thumbnails
 head /home/eprints/feed/bn_urls.txt -n 4 | sed 's/[^0-9]*//g' > /tmp/bn_urls
@@ -11,4 +7,10 @@ head /home/eprints/feed/bn_urls.txt -n 4 | sed 's/[^0-9]*//g' > /tmp/bn_urls
 while read p; do
    runuser -l eprints -c '/opt/eprints3/bin/epadmin redo_thumbnails bnarchives $p --verbose'
 done </tmp/bn_urls
+
+
+runuser -l eprints -c '/opt/eprints3/bin/generate_views bnarchives'
+runuser -l eprints -c '/opt/eprints3/bin/generate_abstracts bnarchives'
+runuser -l eprints -c '/opt/eprints3/bin/generate_static bnarchives'
+
 
