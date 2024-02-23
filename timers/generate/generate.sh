1306b3d5c077739d2295e7c122a85289debddb9e
blairfix <blairfix@fastmail.com>
2024-02-23 16:22:36 -0700

    fixed quotes

--- timers/generate/generate.sh
+++ timers/generate/generate.sh
@@ -5,7 +5,7 @@
 head /home/eprints/feed/bn_urls.txt -n 4 | sed 's/[^0-9]*//g' > /tmp/bn_urls
 
 while read p; do
-   runuser -l eprints -c '/opt/eprints3/bin/epadmin redo_thumbnails bnarchives $p --verbose'
+   runuser -l eprints -c "/opt/eprints3/bin/epadmin redo_thumbnails bnarchives $p --verbose"
 done </tmp/bn_urls
 
 
