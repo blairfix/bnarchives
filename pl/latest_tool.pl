207350a1ce00b0be764a16f5333f71ee77f4f2cd
blairfix <blairfix@fastmail.com>
2024-02-25 20:00:27 -0700

    latest algorithm

--- pl/latest_tool.pl
+++ pl/latest_tool.pl
@@ -8,8 +8,20 @@
 #
 ######################################################################
 
+#$c->{latest_tool_modes} = {
+#    default => { citation => "result", max => "200" }
+#};
+
 $c->{latest_tool_modes} = {
-	default => { citation => "result", max => "200" }
+	default => { citation => "result" },
+	fplatest => { citation => "result", max => 3 }
+};
+
+# Make sure the appropriate Export plugins are also enabled.
+$c->{latest_tool_feeds} = {
+#       Atom => { enabled => 1, "label" => "Atom" },
+#       RSS => { enabled => 1, "label" => "RSS 1.0" },
+        RSS2 => { enabled => 1, "label" => "RSS 2.0" },
 };
 
 # Example of a latest_tool mode. This makes a mode=articles option
