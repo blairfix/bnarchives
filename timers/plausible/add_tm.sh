9629828e051c6e6e2f88d5c195cde9a120fc6f33
blairfix <blairfix@fastmail.com>
2024-04-12 06:46:16 -0600

    plausible startup timer

---
+++ timers/plausible/add_tm.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+cp *.timer *.service /etc/systemd/system/
+
+systemctl daemon-reload
+systemctl enable indexer.timer
+systemctl start indexer.timer
+systemctl restart indexer.timer
+
